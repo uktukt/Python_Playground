22837747e197b15b1b1fdede92bb0a3e78f32542
uktukt <67184340+uktukt@users.noreply.github.com>
2023-05-09 20:39:34 +0300

    Add files via upload

---
+++ Automated File Sorter.py
@@ -0,0 +1,28 @@
+import os, shutil
+
+path = r'C:/Users/dovil/Desktop/testinis/'
+
+file_name = os.listdir(path)
+
+folder_names = ['csv files', 'image files', 'text files', 'pdf files']
+
+for name in range(len(folder_names)):
+    if not os.path.exists(path + folder_names[name]):
+        print(path + folder_names[name])
+        os.makedirs((path + folder_names[name]))
+
+file_types = ['.csv', '.png', 'jpeg', '.txt', '.pdf']
+
+for file in file_name:
+    if '.csv' in file and not os.path.exists(path + 'csv files/' + file):
+        shutil.move(path + file, path + 'csv files/' + file)
+    elif '.png' in file and not os.path.exists(path + 'image files/' + file):
+        shutil.move(path + file, path + 'image files/' + file)
+    elif '.jpeg' in file and not os.path.exists(path + 'image files/' + file):
+        shutil.move(path + file, path + 'image files/' + file)
+    elif '.txt' in file and not os.path.exists(path + 'text files/' + file):
+        shutil.move(path + file, path + 'text files/' + file)
+    elif '.pdf' in file and not os.path.exists(path + 'pdf files/' + file):
+        shutil.move(path + file, path + 'pdf files/' + file)
+
+print(file_name)
